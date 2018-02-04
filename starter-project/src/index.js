a6e8b305d15165a9d10219b7d95188fc48ab6bd7
Chris Esplin <chris@chrisesplin.com>
2018-02-04 01:23:51 +0000

    Starting out

---
+++ starter-project/src/index.js
@@ -0,0 +1,3 @@
+import registerCustomElement from 'preact-custom-element';
+import FirebaseAuthentication from '@quiver/firebase-authentication';
+registerCustomElement(FirebaseAuthentication, 'firebase-authentication');
